f5ce63d3ac0a6c89f258ee36bc59662d6b909bdf
Nguyen Thi An <nguyen.thi.an@framgia.com>
2016-11-14 11:13:41 +0700

    Chap 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,9 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
- 
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
   end
 
